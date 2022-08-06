e6a6cfaf479ef37ceefd9c8fa9a762130b89d5da
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-08-06 20:01:27 +0530

    format some code and readme guide added

--- App.js
+++ App.js
@@ -7,5 +7,5 @@ import "react-native-gesture-handler";
 import Navigator from "./routes/homeStack";
 
 export default function App() {
-  return <Navigator />;
+    return <Navigator />;
 }
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -4,23 +4,23 @@ import getStarted from "../components/GetStarted";
 import login from "../components/Login";
 import OtpVer from "../components/Otpver";
 const screens = {
-  // * SYNTAX GUIDE
-  /* *
-   * navigatorName: {
-   *    screen: componentName,
-   * }
-   *
-   */
+    // * SYNTAX GUIDE
+    /* *
+     * navigatorName: {
+     *    screen: componentName,
+     * }
+     *
+     */
 
-  GetStarted: {
-    screen: getStarted,
-  },
-  Login: {
-    screen: login,
-  },
-  Otpver: {
-    screen: OtpVer,
-  },
+    GetStarted: {
+        screen: getStarted,
+    },
+    Login: {
+        screen: login,
+    },
+    Otpver: {
+        screen: OtpVer,
+    },
 };
 
 const HomeStack = createStackNavigator(screens);
