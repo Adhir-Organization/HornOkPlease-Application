132448a39b21b70f7591aa228aad927608bb0df0
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-09-27 17:30:58 +0530

    fixed a naming convention in dashboard

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Dshboard() {
+export default function Dashboard() {
     return (
         <SafeAreaView style={styles.global}>
             <SafeAreaView style={styles.tripData}>
