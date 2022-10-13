9bcf80f084db323b37dc30c1ca0f4d47d6977613
Vaidik Modi <modivaidik408@gmail.com>
2022-10-14 03:48:08 +0530

    merge changes of contacts

---
+++ src/components/contancts/HeroBox.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Linking, TouchableOpacity } from "react-native";
+import {
+  Box,
+  Text,
+  Center,
+  ScrollView,
+  VStack,
+  HStack,
+  IconButton,
+  Flex,
+} from "native-base";
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default function HeroBox  (props) {
+    return (
+      <Box
+        flex={1}
+        background="#E0DCDC"
+        p="2"
+        mt="6"
+        ml="6"
+        mr="6"
+        borderRadius="13"
+      >
+        <HStack>
+          <Box pt="1">
+            <Icon
+              name="person-circle-sharp"
+              size={50}
+              style={{ color: "#FFBE17" }}
+            />
+          </Box>
+
+          <Box flexDirection="row">
+            <VStack>
+              <Box w="40">
+                <Text fontSize="md" fontWeight="medium" pt={1} paddingLeft={4}>
+                  {props.name}
+                </Text>
+                <Text fontSize="xs" pl={5}>
+                  {props.number}
+                </Text>
+              </Box>
+            </VStack>
+            <TouchableOpacity>
+              <Box pl="20" pt="3">
+                <Icon
+                  name="call-sharp"
+                  size={28}
+                  onPress={() => {
+                    Linking.openURL(`tel:${props.number}`);
+                  }}
+                />
+              </Box>
+            </TouchableOpacity>
+          </Box>
+        </HStack>
+      </Box>
+    );
+  };
--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -11,58 +11,12 @@ import {
   Flex,
 } from "native-base";
 import Icon from "react-native-vector-icons/Ionicons";
+import HeroBox from "../components/contancts/HeroBox";
 export default function Contacts() {
   const Header = () => {
     return <Box background="#0058DB">Contacts</Box>;
   };
 
-  const HeroBox = (props) => {
-    return (
-      <Box
-        flex={1}
-        background="#E0DCDC"
-        p="2"
-        mt="6"
-        ml="6"
-        mr="6"
-        borderRadius="13"
-      >
-        <HStack>
-          <Box pt="1">
-            <Icon
-              name="person-circle-sharp"
-              size={50}
-              style={{ color: "#FFBE17" }}
-            />
-          </Box>
-
-          <Box flexDirection="row">
-            <VStack>
-              <Box w="40">
-                <Text fontSize="md" fontWeight="medium" pt={1} paddingLeft={4}>
-                  {props.name}
-                </Text>
-                <Text fontSize="xs" pl={5}>
-                  {props.number}
-                </Text>
-              </Box>
-            </VStack>
-            <TouchableOpacity>
-              <Box pl="20" pt="3">
-                <Icon
-                  name="call-sharp"
-                  size={28}
-                  onPress={() => {
-                    Linking.openURL(`tel:${props.number}`);
-                  }}
-                />
-              </Box>
-            </TouchableOpacity>
-          </Box>
-        </HStack>
-      </Box>
-    );
-  };
   return (
     <Box safeArea>
       <VStack space={5}>
