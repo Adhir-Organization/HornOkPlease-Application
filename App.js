e5080321535c77069c0e5c83100b5d218c6e6e76
Vaidik Modi <modivaidik408@gmail.com>
2022-10-20 15:04:28 +0530

    created different file  navigation hierarchy set

--- App.js
+++ App.js
@@ -2,36 +2,12 @@
 import "react-native-gesture-handler";
 
 // Packages Import
-import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider, extendTheme, useTheme } from "native-base";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import {
-  createStackNavigator,
-  CardStyleInterpolators,
-} from "@react-navigation/stack";
-
+import MainStackNavigation from "./src/Navigation/MainStackNavigation";
 /* A provider for redux. */
 import { Provider, useDispatch, useSelector } from "react-redux";
 import store from "./src/store/store";
 
-// Icons import
-import Icon from "react-native-vector-icons/Feather";
-
-// Screens Import
-import Dashboard from "./src/screens/Dashboard";
-import PT1StartCheck from "./src/screens/PreTripCheckup/PT1StartCheck";
-import Docs from "./src/screens/Docs";
-import Notifications from "./src/screens/Notifications";
-import ExpenseEntry from "./src/screens/ExpenseEntry";
-import Contacts from "./src/screens/Contacts";
-import GetStarted from "./src/screens/GetStarted";
-import Login from "./src/screens/Login";
-import OtpVerification from "./src/screens/OtpVerification";
-import { login } from "./src/store/authSlice";
-import DriverMaintenance from "./src/screens/DriverMaintenance";
-import PTNavigation from "./src/components/dashboard/PTNavigation.jsx";
-const Tab = createMaterialBottomTabNavigator();
-const Stack = createStackNavigator();
 const theme = extendTheme({
   colors: {
     customClrs: {
@@ -47,84 +23,12 @@ const theme = extendTheme({
   },
 });
 
-const StackNavigation = () => {
-  const items = useSelector((state) => state.auth);
-  console.log(items.isLoggedIn);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-        }}
-        initialRouteName={items.isLoggedIn ? "TabNavigation" : "GetStarted"}
-      >
-        <Stack.Screen name="GetStarted" component={GetStarted} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="OtpVerification" component={OtpVerification} />
-        <Stack.Screen name="TabNavigation" component={TabNavigation} />
-        <Stack.Screen name="Contacts" component={Contacts} />
-        <Stack.Screen name="Notifications" component={Notifications} />
-        <Stack.Screen name="ExpenseEntry" component={ExpenseEntry} />
-        <Stack.Screen name="DriverMaintenance" component={DriverMaintenance} />
-        <Stack.Screen name="PTNavigation" component={PTNavigation} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-const TabNavigation = () => {
-  return (
-    <Tab.Navigator
-      barStyle={{
-        backgroundColor: "#deecff",
-        paddingVertical: 5,
-        borderRadius: 10,
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Dashboard}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: () => <Icon name="home" size={24} />,
-        }}
-      />
-      <Tab.Screen
-        name="Docs"
-        component={Docs}
-        options={{
-          tabBarLabel: "Docs",
-          tabBarIcon: () => <Icon name="file" size={24} />,
-        }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={Notifications}
-        options={{
-          tabBarLabel: "Notification",
-          tabBarIcon: () => <Icon name="bell" size={24} />,
-        }}
-      />
-      <Tab.Screen
-        name="Contacts"
-        component={Contacts}
-        options={{
-          tabBarLabel: "Contacts",
-          tabBarIcon: () => <Icon name="phone" size={24} />,
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
-
 export default function App() {
   return (
     <>
       <Provider store={store}>
         <NativeBaseProvider useTheme={theme}>
-          <StackNavigation>
-            <StackNavigation />
-          </StackNavigation>
+          <MainStackNavigation />
         </NativeBaseProvider>
       </Provider>
     </>
--- src/screens/PreTripCheckup/PT1StartCheck.js
+++ src/screens/PreTripCheckup/PT1StartCheck.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TripCheckupNavigation from "../../components/dashboard/PTNavigation.jsx";
+
 import { Box, Text } from "native-base";
 export default function PT1StartCheck() {
   return (
