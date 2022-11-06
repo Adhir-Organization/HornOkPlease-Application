7b74f29bd0e214cf02f48b395a1e5c1e4ebc7e15
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2022-11-06 11:32:25 +0530

    fixing pt navigation

--- src/Navigation/PTNavigation.js
+++ src/Navigation/PTNavigation.js
@@ -1,21 +1,74 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { StatusBar } from "native-base";
+import { useEffect, useState } from "react";
 import PT1StartCheck from "../screens/PreTripCheckup/PT1StartCheck";
 import PT2CheckUpList from "../screens/PreTripCheckup/PT2CheckUpList";
+import PT3IndividualCheckup from "../screens/PreTripCheckup/PT3IndividualCheckup";
+import PreTripContext from "../context/preTripContext";
+
 export default function PTNavigation() {
   const Stack = createStackNavigator();
+  const [parts, setParts] = useState(null);
+  const truckParts = [
+    {
+      name: "Engine",
+      status: false,
+    },
+    {
+      name: "Fuel System",
+      status: false,
+    },
+    {
+      name: "Air Lines",
+      status: false,
+    },
+    {
+      name: "Battery",
+      status: false,
+    },
+    {
+      name: "Brakes",
+      status: false,
+    },
+    {
+      name: "Clutch",
+      status: false,
+    },
+    {
+      name: "Gear Box",
+      status: false,
+    },
+    {
+      name: "Body",
+      status: false,
+    },
+  ];
+  useEffect(() => {
+    setParts(truckParts);
+    console.log(parts);
+  }, []);
 
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerStyle: {
-          backgroundColor: "#2871E6",
-        },
-        headerTintColor: "#fff",
-      }}
-    >
-      <Stack.Screen name="PT1StartCheck" component={PT1StartCheck} />
-      <Stack.Screen name="PT2CheckUpList" component={PT2CheckUpList} />
-    </Stack.Navigator>
+    <>
+      <StatusBar backgroundColor="#2871E6" barStyle="light-content" />
+      <PreTripContext.Provider value={{ parts, setParts }}>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: true,
+            headerStyle: {
+              backgroundColor: "#2871E6",
+            },
+            headerTintColor: "#fff",
+          }}
+        >
+          <Stack.Screen name="PT1StartCheck" component={PT1StartCheck} />
+          <Stack.Screen name="PT2CheckUpList" component={PT2CheckUpList} />
+          <Stack.Screen
+            name="IndividualCheckups"
+            component={PT3IndividualCheckup}
+          />
+        </Stack.Navigator>
+      </PreTripContext.Provider>
+    </>
   );
 }
---
+++ src/screens/PreTripCheckup/PT3IndividualCheckup.js
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import { Box, Text, VStack, Input, Button, Heading, HStack } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+import PreTripContext from "../../context/preTripContext";
+
+export default function PT3IndividualCheckup({ route }) {
+  const { parts, setParts } = useContext(PreTripContext);
+  const { data } = route.params;
+  const navigation = useNavigation();
+  return (
+    <Box safeArea>
+      <VStack space="2" alignItems={"center"} justifyContent="center">
+        <Heading size="xl" mt="12" mb="3">
+          {data.name}
+        </Heading>
+        <Text>Current Status : {data.status}</Text>
+        <Text textAlign={"center"}>
+          Examine the {data.name.toLowerCase()} and verify that there are no
+          defects and properly working
+        </Text>
+        <Box background={"#FCD4D4"} px="3" py="1" rounded="lg">
+          <Text color="#FF3E3E">Required</Text>
+        </Box>
+        <Box background={"#E1E1E1"} h="300" w="300" rounded="lg" mt="7">
+          <Text>Photo</Text>
+        </Box>
+        <HStack space="2" mt="7">
+          <Button onPress={() => navigation.navigate("PT2CheckUpList")}>
+            Broken
+          </Button>
+          <Button
+            // https://bobbyhadz.com/blog/react-update-state-array-of-objects#:~:text=To%20update%20an%20array%20of,object%20that%20matches%20the%20condition.
+            onPress={() => {
+              // navigation.navigate("PT2CheckUpList");
+              // setParts(...State,)
+              setParts((current) => {
+                // console.log(current);
+                current.map((item) => {
+                  if (item.name == data.name) {
+                    return { ...item, status: true };
+                  }
+                  return item;
+                });
+              });
+            }}
+          >
+            Good
+          </Button>
+        </HStack>
+      </VStack>
+    </Box>
+  );
+}
