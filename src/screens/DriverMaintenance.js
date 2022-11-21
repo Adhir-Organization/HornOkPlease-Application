45759aac065c56b7acf0e163f220df2a14eede7f
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-11-21 13:09:33 +0530

    Date Picker Fixed

--- src/screens/DriverMaintenance.js
+++ src/screens/DriverMaintenance.js
@@ -1,39 +1,54 @@
-import { Box, VStack, Input } from "native-base";
-
-import DateSelector from "../components/maintenance/DateSelector";
+import {
+  Box,
+  VStack,
+  Input,
+  Button,
+  Text,
+  HStack,
+  Pressable,
+  FormControl,
+} from "native-base";
 import IssueSelector from "../components/maintenance/IssueSelector";
+import DateSelector from "../components/maintenance/DateSelector";
+import { useState } from "react";
 
 export default function DriverMaintenance(navigation) {
+  const [todayDate, setTodayDate] = useState();
+  const [doneDate, setDoneDate] = useState();
+
   return (
     <>
-      <Box flex={1} safeArea>
-        <VStack space="3" alignItems="center">
-          {/* Date Picker */}
-          <Box>
-            <DateSelector />
-          </Box>
-          {/* Select Issue */}
-          <Box>
-            <IssueSelector />
-          </Box>
-          {/* Amount Input */}
-          <Box>
-            <Input placeholder="Enter Amount" />
-          </Box>
-          {/* Done date picker */}
-          <Box>
-            <DateSelector />
-          </Box>
-          {/* Garage Name */}
-          <Box>
-            <Input placeholder="Default Input" />
-          </Box>
+      <Box flex="1" safeArea>
+        <Box p={5}>
+          <FormControl isRequired isInvalid>
+            <VStack space="5">
+              {/* Date Picker */}
+              <DateSelector />
+              {/* Select Issue */}
+              <Box>
+                <IssueSelector />
+              </Box>
+              {/* Amount Input */}
+              <Box>
+                <FormControl.Label>Enter Amount</FormControl.Label>
+                <Input placeholder="5000" />
+              </Box>
+              {/* Done date picker */}
+              <Box>
+                <DateSelector />
+              </Box>
+              {/* Garage Name */}
+              <Box>
+                <Input placeholder="Default Input" />
+              </Box>
 
-          {/* Remark Audio Record */}
-          <Box></Box>
-          {/* Submit Button */}
-          <Box></Box>
-        </VStack>
+              {/* Remark Audio Record */}
+              <Box></Box>
+              {/* Submit Button */}
+              <Box></Box>
+            </VStack>
+          </FormControl>
+        </Box>
       </Box>
     </>
   );
