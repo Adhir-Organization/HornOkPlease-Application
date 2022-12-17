283514a64f32655488a56f626bd582c8968b51b5
Vanshika <vanshikag0111@gmail.com>
2022-12-17 14:30:23 +0530

    json and js files for LanguageSupport

---
+++ assets/localization/gj.js
@@ -0,0 +1,19 @@
+import languagekeys from "./languagekeys";
+
+const gj = {
+
+    [languagekeys.helloeveryone]: 'બધાને હેલો',
+
+    [languagekeys.demomultisupport]: "આ રિએક્ટ-નેટિવમાં મલ્ટી લેંગ્વેજ સપોર્ટનો ડેમો છે",
+
+    [languagekeys.summary]: "સારાંશ",
+
+    [languagekeys.changeLanguage]: "ભાષા બદલો",
+
+    [languagekeys.submit]: "સબમિટ કરો",
+
+    [languagekeys.youcanchange]: "તમે અહીંથી તમારી ભાષા બદલી શકો છો.",
+
+}
+
+export default gj;
---
+++ assets/localization/hi.js
@@ -0,0 +1,19 @@
+import languagekeys from "./languagekeys";
+
+const hin = {
+
+    [languagekeys.helloeveryone]: 'सभी को नमस्कार',
+
+    [languagekeys.demomultisupport]: "यह प्रतिक्रिया-मूल में बहु भाषा समर्थन का डेमो है",
+
+    [languagekeys.summary]: "सारांश",
+
+    [languagekeys.changeLanguage]: "भाषा बदलो",
+
+    [languagekeys.submit]: "प्रस्तुत",
+
+    [languagekeys.youcanchange]: "आप यहां से अपनी भाषा बदल सकते हैं।",
+
+}
+
+export default hin;
