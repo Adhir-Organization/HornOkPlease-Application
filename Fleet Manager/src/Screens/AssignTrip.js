99402aba8d778ca28db4aa8e1c4059f39cfcc510
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-12-24 12:28:41 +0530

    done assign trip

--- Fleet Manager/src/Screens/AssignTrip.js
+++ Fleet Manager/src/Screens/AssignTrip.js
@@ -11,6 +11,7 @@ import {
     Button,
     FormControl,
     Alert,
+    ScrollView,
 } from "native-base";
 import { useState } from "react";
 import IssueSelector from "../../../src/components/maintenance/IssueSelector";
@@ -21,29 +22,33 @@ const AssignTrip = () => {
     const [todayDate, setTodayDate] = useState();
     return (
         <>
-        <Box safeArea>
-            <Box background="#0058DB" padding="3">
-                <HStack>
-                    <Center>
-                        <Text fontSize="xl" color="white" mx={["110px"]}>
-                            Available Trucks
-                        </Text>
-                    </Center>
-                </HStack>
-            </Box>
-
-            <Box padding="3">
-                <HStack>
-                    <Text fontSize="xl" fontWeight={"bold"} color="black" mx="6">
-                        Available Trucks
-                    </Text>
-                </HStack>
+            <Box safeArea>
+                <Box background="#0058DB" padding="3">
+                    <HStack>
+                        <Center>
+                            <Text fontSize="xl" color="white" mx={["110px"]}>
+                                Available Trucks
+                            </Text>
+                        </Center>
+                    </HStack>
                 </Box>
-                
-        
+
+                <ScrollView>
+                    <Box padding="3">
+                        <HStack>
+                            <Text fontSize="xl" fontWeight={"bold"} color="black" mx="6">
+                                Available Trucks
+                            </Text>
+                        </HStack>
+                    </Box>
                     <Box p={5}>
                         <FormControl isRequired isInvalid>
                             <VStack space="5">
+                                <Box>
+                                    <FormControl.Label>Vehicle number</FormControl.Label>
+                                    <Input placeholder="GJ10AB9999" />
+                                </Box>
+
                                 {/* Date Picker */}
                                 <Box>
                                     <FormControl.Label>Select Date</FormControl.Label>
@@ -63,34 +68,55 @@ const AssignTrip = () => {
                                         />
                                     </HStack>
                                 </Box>
-                                {/* Select Issue */}
-                                <Box>
-                                    <IssueSelector />
-                                </Box>
-                                {/* Amount Input */}
-                                <Box>
-                                    <FormControl.Label>Enter Amount</FormControl.Label>
-                                    <Input placeholder="5000" />
-                                </Box>
-                                {/* Done date picker */}
-                                <Box>
-                                    <DateSelector />
-                                </Box>
-                                {/* Garage Name */}
-                                <Box>
-                                    <Input placeholder="Default Input" />
-                                </Box>
 
-                                {/* Remark Audio Record */}
-                                <Box></Box>
-                                {/* Submit Button */}
-                                <Box></Box>
+                                <Box>
+                                    <FormControl.Label>From</FormControl.Label>
+                                    <Input placeholder="Jamnagar" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>To</FormControl.Label>
+                                    <Input placeholder="Junagadh" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Duration</FormControl.Label>
+                                    <Input placeholder="3 Days" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Diesel</FormControl.Label>
+                                    <Input placeholder="155L" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Advance Amount</FormControl.Label>
+                                    <Input placeholder="RS. 5000" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Flat</FormControl.Label>
+                                    <Input placeholder="RS. 5000" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Sort Halt</FormControl.Label>
+                                    <Input placeholder="Numbers of hours" />
+                                </Box>
+                                <Box>
+                                    <FormControl.Label>Long Halt</FormControl.Label>
+                                    <Input placeholder="Numbers of hours" />
+                                </Box>
+                                <Box marginBottom="12" display="flex" mx="auto" >
+                                    <Box display="flex" flexDirection="row" >
+                                    <FormControl.Label>Estimate Kms</FormControl.Label>
+                                    <Input placeholder="Numbers of hours" width="40" />
+                                    </Box>
+                                    
+                                    <Box display="flex" flexDirection="row">
+                                        <FormControl.Label>Estimate Kms</FormControl.Label>
+                                        <Input placeholder="Numbers of hours" width="40" />
+                                    </Box>
+                                </Box>
                             </VStack>
                         </FormControl>
                     </Box>
-
-
-        </Box>
+                </ScrollView>
+            </Box>
         </>
     );
 };
