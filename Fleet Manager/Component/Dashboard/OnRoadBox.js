b90dca2b72a90f5c4800aad598444604bd1e9c32
Vaidik Modi <modivaidik408@gmail.com>
2023-01-02 19:51:35 +0530

    fixed

--- Fleet Manager/Component/Dashboard/OnRoadBox.js
+++ Fleet Manager/Component/Dashboard/OnRoadBox.js
@@ -1,73 +1,72 @@
 import {
-    Box,
-    Center,
-    Text,
-    HStack,
-    IconButton,
-    VStack,
-    Button,
+  Box,
+  Center,
+  Text,
+  HStack,
+  IconButton,
+  VStack,
+  Button,
 } from "native-base";
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
 import OnroadTrucks from "../../src/Screens/OnroadTrucks";
 import { useNavigation } from "@react-navigation/native";
 
-const OnRoadBox = ({ truckName, truckId, icon, from, to, screenNavigation, color, btnColor,update }) => {
-    const navigation = useNavigation();
+const OnRoadBox = ({
+  truckName,
+  truckId,
+  icon,
+  from,
+  to,
+  screenNavigation,
+  color,
+  btnColor,
+  update,
+}) => {
+  const navigation = useNavigation();
 
-    return (
-        <Box
-            padding="5"  
-            mx="3"      
-            my="3"
-            backgroundColor={color}
-            borderRadius="9"
-        >
-            <HStack>
-                <IconButton
-                    icon={<Icon name={icon} color={"white"} size={27} />}
-                    variant="unstyled"
-                    rounded="full"
-                    backgroundColor={btnColor}
-                    size="16"
-                />
-                <Box mx="6">
-                    <VStack space="1">
-                        <HStack space="2">
-                        <Text fontSize={24} mb={-1}>
-                            {truckName}
-                        </Text>
-                        <Text marginTop="2">
-                            {update}
-                        </Text>
-
-                        </HStack>
-                        <Text fontSize={16} fontWeight="medium">
-                            {truckId}
-                        </Text>
-                    </VStack>
-                </Box>
-                
+  return (
+    <Box padding="5" mx="3" my="3" backgroundColor={color} borderRadius="9">
+      <HStack>
+        <IconButton
+          icon={<Icon name={icon} color={"white"} size={27} />}
+          variant="unstyled"
+          rounded="full"
+          backgroundColor={btnColor}
+          size="16"
+        />
+        <Box mx="6">
+          <VStack space="1">
+            <HStack space="2">
+              <Text fontSize={24} mb={-1}>
+                {truckName}
+              </Text>
+              <Text marginTop="2">{update}</Text>
             </HStack>
-           
-            <HStack space="32" marginTop={3} >
-                <Text fontSize={"lg"}>
-                    From: {from}
-                </Text>
-                <Text fontSize={"lg"}>
-                    To: {to}
-                </Text>
-            </HStack>
-            <Box>
-                {/* <Button mt="4" fontSize="xl"
-                    // onPress={navigation.navigate(screenNavigation)}
-                    onPress={screenNavigation}
-                >
-                    go
-                </Button> */}
-            </Box>
+            <Text fontSize={16} fontWeight="medium">
+              {truckId}
+            </Text>
+          </VStack>
         </Box>
-    );
+      </HStack>
+
+      <HStack space="32" marginTop={3}>
+        <Text fontSize={"lg"}>From: {from}</Text>
+        <Text fontSize={"lg"}>To: {to}</Text>
+      </HStack>
+      <Box>
+        <Button
+          mt="4"
+          fontSize="xl"
+          //   onPress={navigation.navigate(screenNavigation)}
+          //   onPress={screenNavigation}
+          onPress={() => navigation.navigate(screenNavigation)}
+        >
+          Go
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default OnRoadBox;
--- Fleet Manager/src/Screens/OnroadTrucks.js
+++ Fleet Manager/src/Screens/OnroadTrucks.js
@@ -1,109 +1,102 @@
 import {
-    Box,
-    Center,
-    Text,
-    HStack,
-    iconButton,
-    VStack,
-    Button,
-    ScrollView,
+  Box,
+  Center,
+  Text,
+  HStack,
+  iconButton,
+  VStack,
+  Button,
+  ScrollView,
 } from "native-base";
 import React from "react";
 import OnRoadBox from "../../Component/Dashboard/OnRoadBox";
 import { useNavigation } from "@react-navigation/native";
 
-
 const OnroadTrucks = () => {
+  const navigation = useNavigation();
+  const available = [
+    {
+      // screenNavigation :navigation.navigate("OnRoadTrucksDetails"),
+      screenNavigation: "OnRoadTrucksDetails",
+      truckName: "Tata",
+      truckId: "5515s54154",
+      icon: "truck",
+      from: "Junagadh",
+      to: "dubai",
+      color: "#D2D2D2",
+      btnColor: "#FFBE17",
+      update: "updated 3 hrs ago",
+    },
 
-    const navigation = useNavigation();
-    const available = [
-        {   
-            screenNavigation :navigation.navigate("OnRoadTrucksDetails"),
-            truckName: "Tata",
-            truckId: "5515s54154",
-            icon: "truck",
-            from: "Junagadh",
-            to: "dubai",
-            color: "#D2D2D2",
-            btnColor:"#FFBE17",
-            update: "updated 3 hrs ago",
-        },
-        
-        {
-            // screenNavigation :"OnRoadTrucksDetails",
-            screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
-            truckName: "Volvo",
-            truckId: "5515s54154",
-            icon: "truck",
-            from: "Junagadh",
-            to: "dubai",
-            color: "#D2D2D2",
-            btnColor: "#FFBE17",
-            update: "updated 3 hrs ago",
-            
-        },
-        {
-            // screenNavigation :"OnRoadTrucksDetails",
-            screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
+    {
+      // screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
+      screenNavigation: "LRDocsFleet",
+      truckName: "Volvo",
+      truckId: "5515s54154",
+      icon: "truck",
+      from: "Junagadh",
+      to: "dubai",
+      color: "#D2D2D2",
+      btnColor: "#FFBE17",
+      update: "updated 3 hrs ago",
+    },
+    {
+      screenNavigation: "ExpenseSummary",
+      // screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
 
-            truckName: "Volvo",
-            truckId: "5515s54154",
-            icon: "truck",
-            from: "Junagadh",
-            to: "dubai",
-            color: "#D2D2D2",
-            btnColor: "#FFBE17",
-            update: "updated 3 hrs ago",
-        },
-        {
-            // screenNavigation :"OnRoadTrucksDetails",
-            screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
+      truckName: "Volvo",
+      truckId: "5515s54154",
+      icon: "truck",
+      from: "Junagadh",
+      to: "dubai",
+      color: "#D2D2D2",
+      btnColor: "#FFBE17",
+      update: "updated 3 hrs ago",
+    },
+    {
+      screenNavigation: "Driver",
+      //   screenNavigation: navigation.navigate("OnRoadTrucksDetails"),
 
-            truckName: "tata",
-            truckId: "5515s54154",
-            icon: "truck",
-            from: "Junagadh",
-            to: "dubai",
-            color: "#D2D2D2",
-            btnColor: "#FFBE17",
-            update: "updated 3 hrs ago",
-        },
-    ];
-    
-    return (
-        
-        
-        <Box safeArea>
-            <Box background="#0058DB" padding="3">
-                <HStack>
-                    <Center>
-                        <Text fontSize="xl" color="white" mx={["115px"]}>
-                            On Road Trucks
-                        </Text>
-                    </Center>
-                </HStack>
-            </Box>
-            <ScrollView>
-            <Box padding="3">
-                <HStack>
-                    <Text fontSize="xl" fontWeight={"bold"} color="black" mx="2" mt={4} mb={-4}>
-                        On Road Trucks  
-                    </Text>
-                </HStack> 
-            </Box>
-            {available.map((available, index) => (
-                <OnRoadBox truckId={available.truckId} truckName={available.truckName} icon={available.icon}
-                    from={available.from}
-                    to={available.to}
-                    color={available.color}
-                    btnColor={available.btnColor}
-                    update={available.update}
-                    screenNavigation={available.screenNavigation}
-                    key={index} />
-            ))}
-            </ScrollView>
-        </Box>
-    );
+      truckName: "tata",
+      truckId: "5515s54154",
+      icon: "truck",
+      from: "Junagadh",
+      to: "dubai",
+      color: "#D2D2D2",
+      btnColor: "#FFBE17",
+      update: "updated 3 hrs ago",
+    },
+  ];
+
+  return (
+    <Box safeArea>
+      <Box background="#0058DB" padding="3">
+        <HStack>
+          <Center>
+            <Text fontSize="xl" color="white" mx={["115px"]}>
+              On Road Trucks
+            </Text>
+          </Center>
+        </HStack>
+      </Box>
+      <ScrollView mb="20" mt="5">
+        {available.map((available, index) => (
+          <OnRoadBox
+            truckId={available.truckId}
+            truckName={available.truckName}
+            icon={available.icon}
+            from={available.from}
+            to={available.to}
+            color={available.color}
+            btnColor={available.btnColor}
+            update={available.update}
+            screenNavigation={available.screenNavigation}
+            key={index}
+          />
+        ))}
+      </ScrollView>
+    </Box>
+  );
 };
 
 export default OnroadTrucks;
