86ee54d93b2bd724ece42ab241c9ef36f64a4c4f
Rupen Bhimani <rupenbhimani109@gmail.com>
2023-01-19 18:45:09 +0530

    update on driver details screen

--- Fleet Manager/src/Screens/Drawer Screens/Add_Driver.js
+++ Fleet Manager/src/Screens/Drawer Screens/Add_Driver.js
@@ -1,11 +1,38 @@
-import { View, Text } from "react-native";
+import {
+  Box,
+  Center,
+  Text,
+  HStack,
+  IconButton,
+  VStack,
+  View,
+  Button,
+  ScrollView,
+  FormControl,
+  Input,
+} from "native-base";
 import React from "react";
 
 const Add_Driver = () => {
   return (
-    <View>
-      <Text>Add_Driver</Text>
-    </View>
+    <Box safeArea>
+      <Box background="#0058DB" padding="3">
+        <HStack>
+          <Center>
+            <Text fontSize="xl" color="white" mx={["125px"]}>
+              Add Contacts
+            </Text>
+          </Center>
+        </HStack>
+      </Box>
+      <ScrollView>
+        <Box>
+          <Text>Deepesh kaala</Text>
+          <Text>GJ06AB1234</Text>
+          <Button>Available</Button>
+        </Box>
+      </ScrollView>
+    </Box>
   );
 };
 
