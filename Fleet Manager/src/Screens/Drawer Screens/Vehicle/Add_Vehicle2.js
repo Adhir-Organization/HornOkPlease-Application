11ff019a12f19e7418cd240e6a46f755225659b5
Vaidik Modi <modivaidik408@gmail.com>
2023-01-21 15:11:34 +0530

    navigation

--- Fleet Manager/src/Screens/Drawer Screens/Vehicle/Add_Vehicle2.js
+++ Fleet Manager/src/Screens/Drawer Screens/Vehicle/Add_Vehicle2.js
@@ -1,10 +1,6 @@
 import { Box } from "native-base";
 import React from "react";
-import InputBo from "./Add_Vehicle1";
+
 export default function Add_Vehicle2() {
-  return (
-    <Box>
-      <InputBo title="ad" />
-    </Box>
-  );
+  return <Box></Box>;
 }
